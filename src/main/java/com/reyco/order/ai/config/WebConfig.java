0888d929bd03022e40313e1278ceb4355d2f2f7d
housihai <18307200213@163.com>
2025-03-21 15:07:26 +0800

    Page Flush 404

--- src/main/java/com/reyco/order/ai/config/WebConfig.java
+++ src/main/java/com/reyco/order/ai/config/WebConfig.java
@@ -1,10 +1,15 @@
 package com.reyco.order.ai.config;
 
+import java.io.IOException;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Component
 public class WebConfig implements WebMvcConfigurer{
@@ -12,7 +17,16 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
 		 registry.addResourceHandler("/**")
-         .addResourceLocations("classpath:/static/");
+         .addResourceLocations("classpath:/static/")
+         .resourceChain(true)
+         .addResolver(new PathResourceResolver() {
+             @Override
+             protected Resource getResource(@NonNull String resourcePath, @NonNull Resource location) throws IOException {
+                 Resource requestedResource = location.createRelative(resourcePath);
+                 return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                         : new ClassPathResource("/static/index.html"); // 如果请求的资源不存在，则返回 index.html
+             }
+         });
 	}
 	@Override
 	public void addCorsMappings(@NonNull CorsRegistry registry) {
